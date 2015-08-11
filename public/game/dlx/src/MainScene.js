14e3855d35a82e9fbb6d428746901e7b6806fd75
Dall <Dall@163.com>
2015-08-11 23:58:18 +0800

    进一步修改,加入了广告

--- public/game/dlx/src/MainScene.js
+++ public/game/dlx/src/MainScene.js
@@ -12,14 +12,7 @@ var MainScene = cc.Scene.extend({
         var again = ccui.helper.seekWidgetByName(layer, "again");
         again.setVisible(false);
         var but = ccui.helper.seekWidgetByName(layer, "button");
-        //var rs = ccui.helper.seekWidgetByName(layer, "rs");
-        //cc.log(rs);
-
-        //var rs = new cc.TextFieldTTF("lkjlkjlkjl");
-        //rs.setPosition(cc.winSize.w/2, cc.winSize.height*0.7);
-        //rs.setFontSize(20);
-        ////rs.setFontColor(cc.color(255,255,255,255));
-        //layer.addChild(rs, 100);
+        var rs = ccui.helper.seekWidgetByName(layer, "jieguo");
 
         var start_time = 0;
         var end_time = 0;
@@ -37,7 +30,7 @@ var MainScene = cc.Scene.extend({
                     cc.log("两次点击的时间 = " + (end_time - start_time));
                     start_time = 0;
                     end_time = 0;
-                    //rs.setStringValue("" + (end_time - start_time));
+                    //rs.setString("" + (end_time - start_time));
 
                 }
                 if(anNum == 0){
@@ -46,8 +39,6 @@ var MainScene = cc.Scene.extend({
 
             }
         }, but);
-
-
     }
 
 });
