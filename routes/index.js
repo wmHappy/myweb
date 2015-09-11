deab80f8f6c1174cd9e8ab5c97beaad2537e4579
Dall <Dall@163.com>
2015-09-11 21:25:12 +0800

    提交

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    //res.redirect('/first/index.html');
-    //res.send("周彬诚,欢迎访问!这是我们工作室的第一个网站");
-    //res.render('index', {title:"gaoxiao"});
-    res.redirect("/game/dlx/index.html");
+    res.redirect("http://sqvdj.cn/game/sl/index.html");
 });
 
 module.exports = router;
